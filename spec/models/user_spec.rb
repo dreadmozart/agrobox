bec49ad2ba0b29a319786a2c491f8222c7626c6a
dreadmozart <dreadmozart@gmail.com>
2014-04-23 22:54:59 +0400

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 
 describe User do
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
+		end
+	end
+
 	before do
 		@user = User.new(name: "Example User", email: "user@example.com",
 						 password: "foobar", password_confirmation: "foobar")
